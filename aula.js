3c4a4ac984e526abb4427bfdae4b645affef7a1b
Ryan de Azevedo <ryanb7241@gmail.com>
2023-05-27 19:52:32 -0300

    treino java Script

---
+++ aula.js
@@ -0,0 +1,13 @@
+let peso = 25;
+let altura = 1.71;
+let personalidade = 'calmo';
+console.log('peso:'+peso);
+console.log('altura:'+altura);
+console.log('personalidade:'+personalidade);
+
+let caracteristica = {
+    peso: 25,
+    altura: 1.71,
+    personalidade: 'calmo'
+
+}
